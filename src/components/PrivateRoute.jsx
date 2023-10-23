f957d6d7285bd87efe1522c333ff3c1ecdb6dc13
Michael Zajer <michael.zajer@gmail.com>
2023-10-23 20:19:55 +1100

    created profile page, added logout functionality, added private routes & useAuthStatus hook

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+import { Outlet, Navigate } from "react-router"
+import {useAuthStatus} from "../hooks/useAuthStatus"
+
+export default function PrivateRoute() {
+    const {loggedIn, checkingStatus} = useAuthStatus();
+    if (checkingStatus) {
+        return <h1>Loading...</h1>
+    }
+    return loggedIn ? <Outlet /> : <Navigate to='/sign-in'/>;
+}
