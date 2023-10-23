d55cc0424335206e1371aaa4dd8adff36a5c9bc9
Michael Zajer <michael.zajer@gmail.com>
2023-10-23 16:48:19 +1100

    added sign up with google auth on OAuth.jsx

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,7 +1,43 @@
-import { FcGoogle } from "react-icons/fc"
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
+import { useNavigate } from "react-router";
+
 export default function OAuth() {
+  const navigate = useNavigate()
+ async function onGoogleClick(){
+    try {
+      const auth = getAuth()
+      const provider = new 
+      GoogleAuthProvider()
+      const result = await signInWithPopup(
+        auth, provider)
+        const user = result.user
+
+        // check user exists?
+
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef)
+
+        if(!docSnap.exists()){
+          await setDoc(docRef, {
+            name: user.displayName,
+            email: user.email,
+            timeStamp: serverTimestamp(),
+          })
+        }
+        navigate('/')
+
+    } catch (error) {
+      toast.error("could not authorise with google")
+    }
+  }
   return (
     <button
+    type="button"
+    onClick={onGoogleClick}
     className="flex items-center
     justify-center 
     w-full bg-red-700
