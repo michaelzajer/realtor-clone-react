10cffe3571cf1c34520a5d50c878aa4c9dd5ce31
Michael Zajer <michael.zajer@gmail.com>
2023-10-25 01:07:04 +1100

    finished listing page functinality

--- src/App.js
+++ src/App.js
@@ -19,13 +19,15 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/profile" element={<PrivateRoute/>}>
-            <Route path="/profile" element={<Profile/>} />
+              <Route path="/profile" element={<Profile/>} />
           </Route>
           <Route path="/sign-in" element={<SignIn/>} />
           <Route path="/sign-up" element={<SignUp/>} />
           <Route path="/ForgotPassword" element={<ForgotPassword/>} />
           <Route path="/offers" element={<Offers/>} />
-          <Route path="/create-listing" element={<CreateListing/>} />
+          <Route path="/create-listing" element={<PrivateRoute/>}>
+              <Route path="/create-listing" element={<CreateListing/>} />
+          </Route>
         </Routes>
       </Router>
       <ToastContainer
